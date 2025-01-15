13358c3f82e5717b93f7f84fba6c9b2edc2244fd
Ivan <chaca360@gmail.com>
2025-01-15 12:36:08 -0500

    fix: actualizacion

--- src/elements/table/Tables.jsx
+++ src/elements/table/Tables.jsx
@@ -4,17 +4,18 @@ export const Tables = ({ title = [], data = [] }) => {
   const [rango, setRango] = useState(0);
 
   useEffect(() => {
-    setRango(title.length);
+    setRango((title.length).toString());
 
     return () => {};
   }, [title.length]);
-  console.log(rango);
+
+  console.log(`grid grid-cols-${rango.toString()} border border-slate-800 `);
 
   return (
     <>
       <div className="border w-[90%] mt-10 ">
         <div
-          className={`grid grid-cols-${rango.toString()} border border-slate-800 `}
+          className={`grid grid-cols-4 border border-slate-800 `}
         >
           {title.map((item, index) => (
             <div key={index} className=" border flex justify-center bg-slate-400">
@@ -34,7 +35,7 @@ export const Tables = ({ title = [], data = [] }) => {
             data.map((row, index) => (
               <div
                 key={index}
-                className={`grid grid-cols-${rango.toString() } ${index%2 ==0&&" bg-slate-50"} border border-slate-400  `}
+                className={`grid grid-cols-${rango.toString()} ${index%2 ==0&&" bg-slate-50"} border border-slate-400  `}
               >
                 {row.map((item, i) => (
                   <div key={i} className={`border-l-[1px]  border-slate-400 flex justify-center `}>
